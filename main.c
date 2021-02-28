3290bd18683517c5efdb0ed26a332875c1c985ee
AndrewLZRV <79762238+AndrewLZRV@users.noreply.github.com>
2021-02-28 11:45:07 +0700

    Update main.c

--- main.c
+++ main.c
@@ -2,5 +2,9 @@
 #include "string.h"
 
 int main() {
+    // Concatenation test
+    char * str1 = "hello ";
+    char * str2 = "world!;
+    printf("%s", string_concat(str1, str2));
     return 0;
 }
